73a25e569192071962164c6cb0adfdafe86d5eb3
Kirk Roerig <mr.possoms@gmail.com>
2022-09-09 08:08:42 -0600

    Improved download script

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
-if [ $VO_DATA_PATH -z ]; then
-
+if [ -z $VO_DATA_PATH ]; then
 echo "Please specify the path for visual odometry datasets:"
 read VO_DATA_PATH
+fi
 
-mkdir -p $VO_DATA_PATH
+# create the directory for the dataset if it doesn't exist
+if [ ! -d $VO_DATA_PATH ]; then
+	mkdir -p $VO_DATA_PATH
+fi
+
 cd $VO_DATA_PATH
 
-wget https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_calib.zip
-wget https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_poses.zip
-wget https://s3.eu-central-1.amazonaws.com/avg-kitti/devkit_odometry.zip
-wget https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_gray.zip
+function download_and_unzip {
+	local URL=$1
+	local DATA_ZIP=$(basename $URL)
+	local DATA_DIR=$(basename $URL .zip)
+
+	if [ ! -f $DATA_ZIP ] && [ ! -d $DATA_DIR ]; then
+		# there is no zip or directory, start from the beginning
+		wget --continue $URL
+	fi
+
+	if [ -f $DATA_ZIP ]; then
+		unzip -n $DATA_ZIP -d $DATA_DIR
+	fi
+}
+
+# Download datasets and unzip them
+download_and_unzip https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_calib.zip
+download_and_unzip https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_poses.zip
+download_and_unzip https://s3.eu-central-1.amazonaws.com/avg-kitti/devkit_odometry.zip
+download_and_unzip https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_gray.zip
+
 
 # Add environment variable to conda environment
 cd $CONDA_PREFIX
@@ -20,10 +41,9 @@ mkdir -p ./etc/conda/activate.d
 mkdir -p ./etc/conda/deactivate.d
 
 echo "#!/bin/sh" > ./etc/conda/activate.d/env_vars.sh
-echo "export VO_DATA_PATH = $VO_DATA_PATH" > ./etc/conda/activate.d/env_vars.sh
+echo "export VO_DATA_PATH=$VO_DATA_PATH" > ./etc/conda/activate.d/env_vars.sh
 echo "#!/bin/sh" > ./etc/conda/deactivate.d/env_vars.sh
 echo "unset VO_DATA_PATH" > ./etc/conda/deactivate.d/env_vars.sh
 
 echo "Setup successful, please reactivate your conda environment"
-fi
 
