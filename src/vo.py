654c91137fc2a7afa6a9b64f4f164bca1ec05307
Kirk Roerig <mr.possoms@gmail.com>
2022-09-12 08:33:24 -0600

    Displacement vectors

--- src/vo.py
+++ src/vo.py
@@ -6,12 +6,19 @@ class VO:
 	frames = []
 	descriptors = []
 	keypoints = []
+	matches = []
 
 	orb = cv2.ORB_create()
 	matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
 	def __init__(self):
 		pass
+
+	def reset(self):
+		frames = []
+		descriptors = []
+		keypoints = []
+		matches = []
 
 	def append(self, frame):
 		self.frames += [frame]
@@ -25,10 +32,13 @@ class VO:
 		if len(self.frames) < 2:
 			return
 
-		matches = self.matcher.match(self.descriptors[-2], self.descriptors[-1])
+		# for now, we only keep one set of matches
+		self.matches = [self.matcher.match(self.descriptors[-2], self.descriptors[-1])]
 
-		# roll off old frames
-		self.frames = self.frames[1:]
+
+		if len(self.frames) > 2:		
+			# roll off old frames
+			self.frames = self.frames[1:]
 
 
 
